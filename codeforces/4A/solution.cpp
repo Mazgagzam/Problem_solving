c72b51905a6240e74c7da81618b019f5ad49005a
Mazgagzam <94379179+Mazgagzam@users.noreply.github.com>
2023-08-16 00:45:52 +0600

    Create solution.cpp

---
+++ codeforces/4A/solution.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n(0);
+    cin >> n;
+
+    if (n$2!=0 || n<4) cout << "NO" << '\n';
+    else cout << "YES" << '\n';
+  
+    return 0;
+}
