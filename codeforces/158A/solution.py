c156375e88a390e9776aabd4df6bc537b319f54c
Sourcery AI <>
2023-08-15 19:11:36 +0000

    'Refactored by Sourcery'

--- codeforces/158A/solution.py
+++ codeforces/158A/solution.py
@@ -2,10 +2,5 @@ n, k = map(int, input().split())
 
 a = list(map(int, input().split()))
 
-count = 0
-
-for t in a:
-    if a[k-1] <= t and t>0:
-        count += 1 
-        
+count = sum(1 for t in a if a[k-1] <= t and t>0)
 print(count)
